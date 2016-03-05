73be02c3ec9d113aece05aa7bdc7ef1fd8622bc2
Jeremie Lim <jeremielim@Jeremies-MBP.attlocal.net>
2016-03-04 22:02:24 -0800

    Finished Basic Layout and Transition

---
+++ Codepath-Week5-Assignment/Facebook/NewsFeedViewController.swift
@@ -0,0 +1,52 @@
+//
+//  NewsFeedViewController.swift
+//  Facebook
+//
+//  Created by Timothy Lee on 8/3/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var feedImageView: UIImageView!
+    
+    var selectedImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Configure the content size of the scroll view
+        scrollView.contentSize = CGSizeMake(320, feedImageView.image!.size.height)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        scrollView.contentInset.top = 0
+        scrollView.contentInset.bottom = 50
+        scrollView.scrollIndicatorInsets.top = 0
+        scrollView.scrollIndicatorInsets.bottom = 50
+    }
+    
+    @IBAction func didPressPhoto(sender: UITapGestureRecognizer) {
+        // Set current photo
+        selectedImageView = sender.view as! UIImageView
+        
+        // Perform Segue
+        performSegueWithIdentifier("photoSegue", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        let destinationViewController = segue.destinationViewController as! PhotoViewController
+        
+        destinationViewController.image = selectedImageView.image
+    }
+}
---
+++ Codepath-Week5-Assignment/Facebook/PhotoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PhotoViewController.swift
+//  Facebook
+//
+//  Created by Jeremie Lim on 3/4/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    var image: UIImage!
+    
+    @IBOutlet weak var imageView: UIImageView!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = image
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didTapDone(sender: UITapGestureRecognizer) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
