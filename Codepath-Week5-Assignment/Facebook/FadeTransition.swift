4e7b0181e1502284b9737d292f3b5a54813f170c
Jeremie Lim <jeremielim@Jeremies-MBP.attlocal.net>
2016-03-05 22:05:37 -0800

    added closing animation

--- Codepath-Week5-Assignment/Facebook/FadeTransition.swift
+++ Codepath-Week5-Assignment/Facebook/FadeTransition.swift
@@ -11,11 +11,10 @@ import UIKit
 class FadeTransition: BaseTransition {
     var movingImageView: UIImageView!
     var darkView: UIView!
-    var photoViewController: PhotoViewController!
     
     override func presentTransition(containerView: UIView, fromViewController: UIViewController, toViewController: UIViewController) {
 
-        photoViewController = toViewController as! PhotoViewController
+        let photoViewController = toViewController as! PhotoViewController
         let feedViewController = fromViewController as! NewsFeedViewController
         
         // Hide zoomed photo on destination view
@@ -24,22 +23,17 @@ class FadeTransition: BaseTransition {
         // Hide the original image on the feed view controller
         feedViewController.selectedImageView.alpha = 0
         
-        print(feedViewController.selectedImageView.image?.size.width)
-        
         // Create moving image view
         movingImageView = UIImageView()
         movingImageView.frame = feedViewController.selectedImageView.frame
         movingImageView.image = feedViewController.selectedImageView.image
         movingImageView.clipsToBounds = feedViewController.selectedImageView.clipsToBounds
         
-
         if feedViewController.selectedImageView.image?.size.width == 320 {
             movingImageView.contentMode = .ScaleAspectFill
         } else {
             movingImageView.contentMode = .ScaleAspectFit
         }
-        
-        
         
         containerView.addSubview(movingImageView)
         
@@ -56,7 +50,7 @@ class FadeTransition: BaseTransition {
         
         UIView.animateWithDuration(duration, animations: {
             // Change width and height of zoomed in image
-            self.movingImageView.frame = self.photoViewController.imageView.frame
+            self.movingImageView.frame = photoViewController.imageView.frame
             
             self.darkView.alpha = 0.5
             
@@ -65,25 +59,48 @@ class FadeTransition: BaseTransition {
             self.movingImageView.removeFromSuperview()
             
             // Show zoomed photo
-            self.photoViewController.imageView.alpha = 1
+            photoViewController.imageView.alpha = 1
             self.finish()
         }
     }
     
     override func dismissTransition(containerView: UIView, fromViewController: UIViewController, toViewController: UIViewController) {
+        
+        let photoViewController = fromViewController as! PhotoViewController
         let feedViewController = toViewController as! NewsFeedViewController
         
+        // Hide the current photo
+        photoViewController.imageView.alpha = 0
         
-        self.photoViewController.imageView.alpha = 0
+        // Create moving image view
+        movingImageView = UIImageView()
+        movingImageView.frame = photoViewController.imageView.frame
+        movingImageView.image = photoViewController.imageView.image
+        movingImageView.clipsToBounds = photoViewController.imageView.clipsToBounds
+        
+        if feedViewController.selectedImageView.image?.size.width == 320 {
+            movingImageView.contentMode = .ScaleAspectFill
+        } else {
+            movingImageView.contentMode = .ScaleAspectFit
+        }
+        
+        containerView.addSubview(movingImageView)
         
         fromViewController.view.alpha = 1
         UIView.animateWithDuration(duration, animations: {
+            
             self.movingImageView.frame = feedViewController.selectedImageView.frame
             self.darkView.alpha = 0
             
         }) { (finished: Bool) -> Void in
-            feedViewController.selectedImageView.alpha = 1
+            
+            // Hide moving image
             self.movingImageView.removeFromSuperview()
+            
+            // Show original image
+            feedViewController.selectedImageView.alpha = 1
+            
+            
             self.darkView.removeFromSuperview()
             self.finish()
         }
